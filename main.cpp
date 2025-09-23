625cb7a1a1ccdaf99da88d47b1be812e624c3fa8
Alex <kelheor23@gmail.com>
2025-09-23 16:14:10 +0300

    test pin 5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <wiringPi.h>
 
 using namespace std;
 
+void startWiringPi()
+{
+    if (wiringPiSetup() == -1) {
+        std::cerr << "Ошибка инициализации WiringPi." << std::endl;
+        return;
+    }
+}
+
 int main()
 {
+    startWiringPi();
+    pinMode(5, INPUT);
+    pullUpDnControl(5, PUD_DOWN);
+
+    while (true) {
+        if (digitalRead(5) == HIGH)
+            std::cout << "active" << std::endl;
+    }
     cout << "Hello World!" << endl;
     return 0;
 }
