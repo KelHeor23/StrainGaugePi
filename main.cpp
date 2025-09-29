c66b0b950270505830a55c285921e401ec6b9e56
Alex <kelheor23@gmail.com>
2025-09-29 09:59:07 +0300

    calibration

--- main.cpp
+++ main.cpp
@@ -1,26 +1,111 @@
 #include <iostream>
 #include "hx711/common.h"
 
-int main()
+#include <cmath>
+#include <cstdlib>
+#include <chrono>
+#include <string>
+
+using namespace HX711;
+using namespace std;
+
+std::size_t samples;
+std::string unit;
+double knownWeight;
+Value zeroValue;
+SimpleHX711* hx;
+
+int main(int argc, char** argv)
 {
-    /*startWiringPi();
+    if(argc != 3) {
+        cout << "Usage: hx711calibration [data pin] [clock pin]" << endl;
+        return EXIT_FAILURE;
+    }
 
-    pinMode(5, INPUT);
-    pullUpDnControl(5, PUD_DOWN);
+    try {
+        hx = new SimpleHX711(stoi(argv[1]), stoi(argv[2]), 1, 0);
+    }
+    catch(const GpioException& ex) {
+        cerr << "Failed to connect to HX711 chip" << endl;
+        return EXIT_FAILURE;
+    }
+    catch(const TimeoutException& ex) {
+        cerr << "Failed to connect to HX711 chip" << endl;
+        return EXIT_FAILURE;
+    }
 
-    while (true) {
-        if (digitalRead(5) == HIGH)
-            std::cout << "active" << std::endl;
-    }*/
+    //clear screen
+    cout    << "\x1B[2J\x1B[H"
 
-    using namespace HX711;
-    using std::chrono::seconds;
+         //splash
+         << "========================================" << endl
+         << "HX711 Calibration" << endl
+         << "========================================"
+         << endl << endl
+         << "Find an object you know the weight of. If you can't find anything, "
+         << "try searching Google for your phone's specifications to find its weight. "
+         << "You can then use your phone to calibrate your scale."
+         << endl << endl
 
-    AdvancedHX711 hx(2, 3, -370, -367471, Rate::HZ_80);
+         //unit prompt
+         << "1. Enter the unit you want to measure the object in (eg. g, kg, lb, oz): ";
+    cin >> unit;
+    cin.ignore();
 
-    // constantly output weights using the median of all samples
-    // obtained within 1 second
-    for(;;) std::cout << hx.weight(seconds(1)) << std::endl; //eg. 0.03 g
+    //known weight prompt
+    cout    << endl
+         << "2. Enter the weight of the object in the unit you chose (eg. "
+         << "if you chose 'g', enter the weight of the object in grams): ";
+    cin >> knownWeight;
+    cin.ignore();
 
-    return 0;
+    //samples prompt
+    cout    << endl
+         << "3. Enter the number of samples to take from the HX711 chip (eg. 15): ";
+    cin >> samples;
+    cin.ignore();
+
+    //tare prompt
+    cout    << endl << "4. Remove all objects from the scale and then press enter.";
+    cin.ignore();
+    cout    << endl << "Working..." << flush;
+
+    zeroValue = hx->read(Options(samples));
+
+    //weigh prompt
+    cout    << endl << endl << "5. Place object on the scale and then press enter.";
+    cin.ignore();
+    cout    << endl << "Working..." << flush;
+
+    const double raw = hx->read(Options(samples));
+    const double refUnitFloat = (raw - zeroValue) / knownWeight;
+    Value refUnit = static_cast<Value>(round(refUnitFloat));
+    delete hx;
+
+    if(refUnit == 0) {
+        refUnit = 1;
+    }
+
+    cout    << endl << endl
+         << "Known weight (your object): " << knownWeight << unit << endl
+         << "Raw value over " << samples << " samples: " << raw << endl
+         << endl
+         << "-> REFERENCE UNIT: " << refUnit << endl
+         << "-> ZERO VALUE: " << zeroValue << endl
+         << endl
+         << "You can provide these values to the constructor when you create the "
+         << "HX711 objects or later on. For example: " << endl
+         << endl
+         << "SimpleHX711 hx("
+         << argv[1] << ", " << argv[2] << ", " << refUnit << ", " << zeroValue
+         << ");" << endl
+         << "OR" << endl
+         << "hx.setReferenceUnit("
+         << refUnit
+         << "); and hx.setOffset("
+         << zeroValue
+         << ");" << endl
+         << endl;
+
+    return EXIT_SUCCESS;
 }
